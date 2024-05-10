7bf6186b82beeb5ea49bc398f9cff134d577db72
Petar Kresoja <pkresoja@singidunum.ac.rs>
2024-05-10 16:03:11 +0200

    Podesavanje osnovnog Express projekta

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello World')
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
+
+const port = 4000
+app.listen(port, () => {
+    console.log("App started and listening on " + port)
+})
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello World From ExpressJS and TypeScript!"
+    })
+})
+
+app.get("*", (req, res) => {
+    res.status(404).json({
+        message: "Not found!"
+    })
+})
